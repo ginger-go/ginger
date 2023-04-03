d740abfe19e7408fa523921041ae843c80d9d890
Nelson Lai <nelsonlai9503@gmail.com>
2023-04-03 12:57:37 +0800

    add create mock context

--- context.go
+++ context.go
@@ -1,6 +1,8 @@
 package ginger
 
 import (
+	"net/http/httptest"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ginger-go/sql"
 )
@@ -11,6 +13,51 @@ type Context[T any] struct {
 	Page       *sql.Pagination
 	Sort       *sql.Sort
 	Response   interface{}
+}
+
+type MockContextParams[T any] struct {
+	Request   *T
+	Page      *sql.Pagination
+	Sort      *sql.Sort
+	Method    string
+	Path      string
+	ClientIP  string
+	UserAgent string
+	Headers   map[string]string
+}
+
+func NewMockContext[T any](param MockContextParams[T]) *Context[T] {
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+
+	if param.Method == "" {
+		param.Method = "GET"
+	}
+	if param.Path == "" {
+		param.Path = "/"
+	}
+	ctx.Request = httptest.NewRequest(param.Method, param.Path, nil)
+	if param.ClientIP != "" {
+		ctx.Request.RemoteAddr = param.ClientIP
+	}
+
+	if param.UserAgent != "" {
+		ctx.Request.Header.Set("User-Agent", param.UserAgent)
+	} else {
+		ctx.Request.Header.Set("User-Agent", "GingerMockContext")
+	}
+	if param.Headers != nil {
+		for k, v := range param.Headers {
+			ctx.Request.Header.Set(k, v)
+		}
+	}
+
+	return &Context[T]{
+		GinContext: ctx,
+		Request:    param.Request,
+		Page:       param.Page,
+		Sort:       param.Sort,
+	}
 }
 
 func (ctx *Context[T]) ClientIP() string {
